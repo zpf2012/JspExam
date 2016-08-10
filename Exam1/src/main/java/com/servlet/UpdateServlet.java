b24d638f6909ec7459fec102a9095f050678025b
pengfei.zhang@hand-china.com <pengfei.zhang@hand-china.com>
2016-08-10 18:22:53 +0800

    最终版本

---
+++ Exam1/src/main/java/com/servlet/UpdateServlet.java
@@ -0,0 +1,75 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.impl.UpdateFilmImpl;
+import com.entities.Film;
+import com.fm.CreateHtml;
+import com.util.ConnectionFactory;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+public class UpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = ConnectionFactory.getInstance().makeConn();
+		try {
+			conn.setAutoCommit(false);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		String id = request.getParameter("id");
+		Film film = new Film();
+		film.setFilm_id(Integer.parseInt(id));
+		ResultSet rs = null;
+		UpdateFilmImpl ufi = new UpdateFilmImpl();
+		CreateHtml ch = new CreateHtml();
+		try {
+			rs = ufi.select(conn, film);
+			while(rs.next()){
+				ch.create(rs.getString("title"), rs.getString("description"), rs.getString("name"));
+			}	
+			conn.close();
+			
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("/Exam1/Update.html");
+		
+	}
+
+}
---
+++ Exam1/src/main/webapp/newfilm.jsp
@@ -0,0 +1,31 @@
+<%@page import="com.service.GetLanguage"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@page import="java.sql.ResultSet"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>新增电影</title>
+<%
+	ResultSet rs = new GetLanguage().getlanguage();
+%>
+</head>
+<body>
+
+	<div align="center">
+		<form action="/Exam1/NewFilmServlet" method="post">
+			电影名称：<input type="text" name="title" /><br> <br> 电影描述：<input
+				type="text" name="desc" /><br> <br> 选择语种：<select
+				name="language">
+				<%
+					while (rs.next()) {
+				%>
+				<option value="<%=rs.getString("name")%>"><%=rs.getString("name")%></option>
+				<%
+					}
+				%>
+			</select> <input type="submit" value="提交" />
+		</form>
+	</div>
+</body>
+</html>
